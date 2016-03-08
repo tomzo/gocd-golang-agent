e71de56a4987323832b2975dc9be7defa5047b25
wpc <alex.hal9000@gmail.com>
2016-03-08 00:25:55 -0800

    make gocd-agent run in background

--- installers/deb/build.sh
+++ installers/deb/build.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
-set -x
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 PROJECT_DIR="$SCRIPT_DIR/../../"
+BINARY_FILE="$SCRIPT_DIR/package/usr/bin/gocd-agent"
+DEB_FILE="gocd-agent_0.1-1_all.deb"
+
+echo "############################"
+echo "Cross compiling for linux..."
+echo "############################"
+
 cd $PROJECT_DIR
 CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' .
-mkdir -p $SCRIPT_DIR/DEBIAN/usr/share/bin
-cp gogoagent $SCRIPT_DIR/DEBIAN/usr/share/bin/gocd-agent
-chmod 0755 $SCRIPT_DIR/DEBIAN/usr/share/bin/gocd-agent
+mkdir -p `dirname $BINARY_FILE`
+cp gogoagent $BINARY_FILE
+chmod 0755 $BINARY_FILE
+
+echo "############################"
+echo "Packaging to deb file..."
+echo "############################"
+
 cd $SCRIPT_DIR
 docker build -t gocd/deb-maker .
-docker run -v ${PWD}:/build gocd/deb-maker /bin/bash -c "cd /build/package && fakeroot dpkg-deb --build . ../gocd-agent_0.1-1_all.deb"
+docker run -v ${PWD}:/build gocd/deb-maker /bin/bash -c "cd /build/package && fakeroot dpkg-deb --build . ../$DEB_FILE"
+
+echo "############################"
+echo "Making sure installer can be installed..."
+echo "############################"
+
+docker run -v ${PWD}:/build gocd/deb-maker /bin/bash -c "cd /build && dpkg -i $DEB_FILE && service gocd-agent start && sleep 3 && ps aux | grep -v 'grep' | grep gocd-agent"
+
+echo "All check passed."
