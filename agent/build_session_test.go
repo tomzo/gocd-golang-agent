472a7e55d02d057896d76708549ccf92cb6cb729
Xiao Li <swing1979@gmail.com>
2016-04-01 09:45:28 -0700

    merge echo, exec, report, test commands test into build_session_test

--- agent/build_session_test.go
+++ agent/build_session_test.go
@@ -28,6 +28,22 @@ import (
 	"time"
 )
 
+func TestEcho(t *testing.T) {
+	setUp(t)
+	defer tearDown()
+
+	goServer.SendBuild(AgentId, buildId,
+		protocol.EchoCommand("echo hello world"),
+	)
+	assert.Equal(t, "agent Building", stateLog.Next())
+	assert.Equal(t, "build Passed", stateLog.Next())
+	assert.Equal(t, "agent Idle", stateLog.Next())
+
+	log, err := goServer.ConsoleLog(buildId)
+	assert.Nil(t, err)
+	assert.Equal(t, "echo hello world\n", trimTimestamp(log))
+}
+
 func TestExport(t *testing.T) {
 	setUp(t)
 	defer tearDown()
@@ -63,6 +79,20 @@ setting environment variable 'TEST_EXPORT' to value 'EXPORT_VALUE'
 	assert.Equal(t, expected, trimTimestamp(log))
 }
 
+func TestExecCommand(t *testing.T) {
+	setUp(t)
+	defer tearDown()
+
+	goServer.SendBuild(AgentId, buildId, protocol.ExecCommand("echo", "abcd"))
+
+	assert.Equal(t, "agent Building", stateLog.Next())
+	assert.Equal(t, "build Passed", stateLog.Next())
+	assert.Equal(t, "agent Idle", stateLog.Next())
+
+	log, err := goServer.ConsoleLog(buildId)
+	assert.Nil(t, err)
+	assert.Equal(t, "abcd\n", trimTimestamp(log))
+}
 func TestMkdirCommand(t *testing.T) {
 	setUp(t)
 	defer tearDown()
@@ -243,5 +273,96 @@ func TestShouldFailBuildIfWorkingDirDoesNotExist(t *testing.T) {
 
 	config := GetConfig()
 	expected := Sprintf("ERROR: Working directory \"%v/%v\" is not a directory\n", config.WorkingDir, "notexist/subdir")
+	assert.Equal(t, expected, trimTimestamp(log))
+}
+
+func TestReportStatusAndCompleting(t *testing.T) {
+	setUp(t)
+	defer tearDown()
+
+	goServer.SendBuild(AgentId, buildId,
+		protocol.ReportCurrentStatusCommand("Preparing"),
+		protocol.ReportCurrentStatusCommand("Building"),
+		protocol.ReportCompletingCommand(),
+	)
+
+	assert.Equal(t, "agent Building", stateLog.Next())
+	assert.NotEqual(t, "", GetState("cookie"))
+
+	assert.Equal(t, "build Preparing", stateLog.Next())
+	assert.Equal(t, "build Building", stateLog.Next())
+	assert.Equal(t, "build Passed", stateLog.Next())
+	assert.Equal(t, "build Passed", stateLog.Next())
+
+	assert.Equal(t, "agent Idle", stateLog.Next())
+}
+
+func TestTestCommand(t *testing.T) {
+	setUp(t)
+	defer tearDown()
+
+	wd := createTestProjectInPipelineDir()
+	file := "src/hello/3.txt"
+	dir := "src/hello"
+
+	var tests = []struct {
+		echo     string
+		testArgs []string
+		expected string
+	}{
+		{"file exist", []string{"-f", file}, "file exist\n"},
+		{"file not exist", []string{"-f", file + "no"}, ""},
+		{"dir is not file", []string{"-f", dir}, ""},
+		{"file exist", []string{"-nf", file}, ""},
+		{"file not exist", []string{"-nf", file + "no"}, "file not exist\n"},
+		{"dir is not file", []string{"-nf", dir}, "dir is not file\n"},
+
+		{"dir exist", []string{"-d", dir}, "dir exist\n"},
+		{"dir exist", []string{"-nd", dir}, ""},
+		{"dir not exist", []string{"-d", dir + "no"}, ""},
+		{"dir not exist", []string{"-nd", dir + "no"}, "dir not exist\n"},
+		{"file is not dir", []string{"-d", file}, ""},
+		{"file is not dir", []string{"-nd", file}, "file is not dir\n"},
+
+		{"equal", []string{"-eq", "hello", "echo", "hello"}, "equal\n"},
+		{"not equal", []string{"-eq", "hello", "echo", "world"}, ""},
+		{"equal", []string{"-neq", "hello", "echo", "hello"}, ""},
+		{"not equal", []string{"-neq", "hello", "echo", "world"}, "not equal\n"},
+	}
+
+	for _, test := range tests {
+		testCmd := protocol.TestCommand(test.testArgs...).Setwd(relativePath(wd))
+		goServer.SendBuild(AgentId, buildId, echo(test.echo).SetTest(testCmd))
+		assert.Equal(t, "agent Building", stateLog.Next())
+		assert.Equal(t, "build Passed", stateLog.Next())
+		assert.Equal(t, "agent Idle", stateLog.Next())
+		log, err := goServer.ConsoleLog(buildId)
+		if err != nil {
+			t.Errorf("Can't find console log when test: %+v", test)
+		}
+		actual := trimTimestamp(log)
+		if test.expected != actual {
+			t.Errorf("test: %+v\nbut was '%v'", test, actual)
+		}
+		os.Truncate(goServer.ConsoleLogFile(buildId), 0)
+	}
+}
+
+func TestTestCommandEchoShouldAlsoBeMaskedForSecrets(t *testing.T) {
+	setUp(t)
+	defer tearDown()
+
+	testCmd := protocol.TestCommand("-eq", "$$$", "echo", "secret")
+	goServer.SendBuild(AgentId, buildId,
+		protocol.SecretCommand("secret", "$$$"),
+		protocol.EchoCommand("hello world").SetTest(testCmd),
+	)
+	assert.Equal(t, "agent Building", stateLog.Next())
+	assert.Equal(t, "build Passed", stateLog.Next())
+	assert.Equal(t, "agent Idle", stateLog.Next())
+
+	log, err := goServer.ConsoleLog(buildId)
+	assert.Nil(t, err)
+	expected := Sprintf("hello world\n")
 	assert.Equal(t, expected, trimTimestamp(log))
 }
