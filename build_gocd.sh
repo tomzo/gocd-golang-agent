a24fe9ec7700d7402415221f41ebca7fe61c4808
Barrow Kwan <barrowkwan@gmail.com>
2016-08-09 14:52:48 -0700

    clean up external library and test report xml

--- build_gocd.sh
+++ build_gocd.sh
@@ -32,17 +32,44 @@ if [[ -f gocd-golang-agent ]]; then
 fi
 
 # Pull dependencies
+echo "================"
 echo "Get dependencies"
+echo "================"
+
+if [[ -d src/golang.org/x/net/websocket ]]; then
+  echo "Remove external library : golang.org/x/net/websocket"
+  rm -rf src/golang.org/x/net/websocket
+fi
+echo "Get golang.org/x/net/websocket"
 go get golang.org/x/net/websocket
 
 # Pull dependencies used by Test
+if [[ -d src/golang.org/x/text ]]; then
+  echo "Remove external library : golang.org/x/text"
+  rm -rf src/golang.org/x/text
+fi
 go get golang.org/x/text
+
+if [[ -d src/golang.org/x/crypto/ssh ]]; then
+  echo "Remove external library : golang.org/x/crypto/ssh"
+  rm -rf src/golang.org/x/crypto/ssh
+fi
 go get golang.org/x/crypto/ssh
 
+if [[ -f $GOPATH/bin/go-junit-report ]]; then
+  echo "Remove binary : $GOPATH/bin/go-junit-report"
+  rm -rf $GOPATH/bin/go-junit-report
+fi
 go build -o $GOPATH/bin/go-junit-report github.com/jstemmer/go-junit-report
-go test -test.v github.com/gocd-contrib/gocd-golang-agent | $GOPATH/bin/go-junit-report > testreport.xml
+if [[ -f testreport.xml ]]; then
+  echo "Remove old test report : testreport.xml"
+  /bin/rm -rf testreport.xml
+fi
+go test -test.v github.com/gocd-contrib/gocd-golang-agent... | $GOPATH/bin/go-junit-report > testreport.xml
 
 # Go Build !!
 echo "Starting   building..."
-
-CGO_ENABLED=0 GOOS=darwin go build -a -o gocd-golang-agent github.com/gocd-contrib/gocd-golang-agent
+/bin/rm -rf output
+/bin/mkdir output
+CGO_ENABLED=0 GOOS=linux go build -a -o output/gocd-golang-agent_linux_x86 github.com/gocd-contrib/gocd-golang-agent
+CGO_ENABLED=0 GOOS=darwin go build -a -o output/gocd-golang-agent_darwin_x86 github.com/gocd-contrib/gocd-golang-agent
