7dd4f9274006f0bc295f9fe00c0565cdb16060e2
Barrow Kwan <barrowkwan@gmail.com>
2016-09-12 16:57:04 -0700

    Fix syntax error

--- agent/diskspace_windows.go
+++ agent/diskspace_windows.go
@@ -55,13 +55,15 @@ func diskSpace(path string) (total, free int64, err error) {
 	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
 	// According to MSDN, err will never be nil, r1 = 0 mean call failed
-	r1, _, err = c.Call(
+	r1, _, errCall := c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(path))),
 		uintptr(unsafe.Pointer(&freeBytes)),
 		uintptr(unsafe.Pointer(&totalBytes)),
 		uintptr(unsafe.Pointer(&availBytes)))
 	if r1 != 0 {
 		err = nil
+	}else{
+		err = errCall
 	}
 	total = totalBytes
 	free = freeBytes
